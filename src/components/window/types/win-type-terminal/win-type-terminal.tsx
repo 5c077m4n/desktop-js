02a204c0f8b61627df17d2bb38bd84df9964d673
Roee Shapira <roy.shap1@gmail.com>
2019-11-11 20:46:22 +0200

    Added style files.

--- src/components/window/types/win-type-terminal/win-type-terminal.tsx
+++ src/components/window/types/win-type-terminal/win-type-terminal.tsx
@@ -1,5 +1,6 @@
 import { Component, Host, h, Element } from '@stencil/core';
 import { Terminal } from 'xterm';
+import 'node_modules/xterm/css/xterm.css';
 
 @Component({
 	tag: 'win-type-terminal',
@@ -11,6 +12,7 @@ export class WinTypeTerminal {
 	private term;
 
 	async componentDidLoad() {
+		console.log(Terminal);
 		this.term = new Terminal();
 		this.term.open(this.el);
 		this.term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
